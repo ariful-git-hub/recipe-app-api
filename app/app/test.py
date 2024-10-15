3416f761d32d9c539a94267dffe3eebe979011a4
mohammad_arif <ariful@lexaeon.com>
2024-10-15 21:11:37 +0600

    PsycopG2 added

--- app/app/test.py
+++ app/app/test.py
@@ -1,18 +1,3 @@
 
 
 from django.test import SimpleTestCase
-
-from app import calc
-
-
-class CalcTest(SimpleTestCase):
-     
-    def test_add(self):
-        result = calc.add(5,10)
-        
-        self.assertEqual(result,15)
-        
-    def test_subtract(self):
-        result = calc.subtract(15,10)
-        
-        self.assertEqual(result,5)
